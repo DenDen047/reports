61dd59efdd004d4a5fb0728b1da65957189cd12a
denden047_dng <naoya.muramatsu@digitalnature.slis.tsukuba.ac.jp>
2018-06-28 05:20:17 +0900

    文章要約手法のところを書いている

--- latex.sh
+++ latex.sh
@@ -8,15 +8,19 @@ fi
 cd $1
 
 TexFile="paper"
-TexCmd="
-    uplatex ${TexFile}.tex &&\
-    pbibtex ${TexFile} &&\
-    uplatex ${TexFile}.tex &&\
-    uplatex ${TexFile}.tex &&\
-    dvipdfmx ${TexFile}"
+# === uplatex ===
 # TexCmd="
 #     uplatex ${TexFile}.tex &&\
+#     pbibtex ${TexFile} &&\
+#     uplatex ${TexFile}.tex &&\
+#     uplatex ${TexFile}.tex &&\
 #     dvipdfmx ${TexFile}"
+# === pdflatex ===
+TexCmd="
+    pdflatex ${TexFile}.tex &&\
+    bibtex ${TexFile} &&\
+    pdflatex ${TexFile}.tex &&\
+    pdflatex ${TexFile}.tex"
 
 docker run \
     --rm -it \
